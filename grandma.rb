1accd63df5529879495ed85a99cbc8239053d383
Aaron Chu <aaronchu357@gmail.com>
2019-04-02 22:08:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(shout)
-  if shout == shout.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif shout == "I LOVE YOU GRANDMA!"
+  if shout == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif shout == shout.upcase
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
